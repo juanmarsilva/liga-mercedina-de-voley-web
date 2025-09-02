1236e23c39c04e3e3c6c17dd23f52280dc5b0273
juanmarsilva <jsilva@quaresitsolutions.com>
2025-09-02 19:11:11 -0300

    upd matches get

--- src/app/api/matches/route.ts
+++ src/app/api/matches/route.ts
@@ -1,10 +1,18 @@
+import { prisma } from "@/lib";
+
 import { NextRequest, NextResponse } from "next/server";
 
 const GET = async ( request: NextRequest ) => {
     try {
-        
+        const matches = await prisma.match.findMany({
+            include: {
+                teamA: { select: { description: true } },
+                teamB: { select: { description: true }},
+            }
+        });
+        return NextResponse.json( matches, { status: 200 });
     } catch (error) {
-        
+        return NextResponse.json({ error: 'Hubo un error al buscar los partidos' }, { status: 500 });
     };
 };
 
